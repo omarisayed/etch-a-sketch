e3ad975daced7007cfc3ba2628c2da1ca9959c2d
omarisayed <osayedunoff@gmail.com>
2023-02-15 16:11:35 +0300

    Wrote a function to create and populate the grid.

--- main.js
+++ main.js
@@ -2,9 +2,29 @@ const body = document.body
 const container = document.querySelector('.container');
 const title = document.querySelector('.game-title');
 const grid_div = document.querySelector('.grid');
+const random_btn = document.querySelector('.random-button');
 const color_input = document.querySelector('.color-selector');
 const range_input = document.querySelector('.text-box');
 const size_btn = document.querySelector('.set-size-button');
-const clear_btn = document.querySelector('.clear-button');
+const reset_btn = document.querySelector('.reset-button');
 
-grid_div.style.gridTemplateColumn = 'reapeat(16, 1fr)';
+
+
+
+
+
+
+
+
+function populateGridDiv(size){
+    grid_div.style.gridTemplateColumns = `repeat(${size}, 1fr)`; 
+    grid_div.style.gridTemplateRows = `repeat(${size},1fr)`;
+
+    for (let i = 0; i < size * size ; i++){
+        let box = document.createElement('div')
+        grid_div.append(box)
+    }
+}
+
+
+
