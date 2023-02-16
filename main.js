f811113f5ca54e466681efc178b9b9c65475a5ae
omarisayed <osayedunoff@gmail.com>
2023-02-16 14:11:06 +0300

    Created changeGridSize(), clearGridContainer(), resetGridSize() functions.

--- main.js
+++ main.js
@@ -1,30 +1,49 @@
 const body = document.body
 const container = document.querySelector('.container');
 const title = document.querySelector('.game-title');
-const grid_div = document.querySelector('.grid');
+const grid_container = document.querySelector('.grid-container');
 const random_btn = document.querySelector('.random-button');
-const color_input = document.querySelector('.color-selector');
-const range_input = document.querySelector('.text-box');
-const size_btn = document.querySelector('.set-size-button');
+const select_color = document.querySelector('.color-selector');
+const input_textbox = document.querySelector('.size-textbox');
+const submit_btn = document.querySelector('.submit-button');
 const reset_btn = document.querySelector('.reset-button');
 
+//event listeners
+
+reset_btn.addEventListener("click", resetGridSize);
 
 
 
+// functions
 
+function populateGridContainer(size) {
+    grid_container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid_container.style.gridTemplateRows = `repeat(${size},1fr)`;
 
-
-
-
-function populateGridDiv(size){
-    grid_div.style.gridTemplateColumns = `repeat(${size}, 1fr)`; 
-    grid_div.style.gridTemplateRows = `repeat(${size},1fr)`;
-
-    for (let i = 0; i < size * size ; i++){
+    for (let i = 0; i < size * size; i++) {
         let box = document.createElement('div')
-        grid_div.append(box)
+        grid_container.append(box)
     }
 }
 
+function changeGridSize(value) {
+    if ((value < 1) || (value > 100)){
+        window.alert('You can only insert between 1 and 100!');
+    }else{
+        clearGridContainer(); // each time I submit a new size, the grid container gets cleared first until it has no existing boxes left. Then I populate the grid container.
+        populateGridContainer(value);
+    }
+}
 
+function clearGridContainer() {
+    let boxes = grid_container.querySelectorAll('div')
+    boxes.forEach(function deleteBoxes(div) {   //remove each div
+        div.remove();
+    })
+}
+
+function resetGridSize(){
+    populateGridContainer(16);  // I choose 16 to be the default size when grid container is reset
+    input_textbox.value = '16'
+}
 
